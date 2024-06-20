5124c6a58864770c84196b423afdd53ee65aefab
harshNileshDesai <146313952+harshNileshDesai@users.noreply.github.com>
2024-06-20 11:36:14 +0530

    initialized academic360

---
+++ code/backend/academic360-backend/src/main/java/com/academic360/Academic360BackendApplication.java
@@ -0,0 +1,13 @@
+package com.academic360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Academic360BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Academic360BackendApplication.class, args);
+	}
+
+}
---
+++ code/backend/academic360-backend/src/test/java/com/academic360/Academic360BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Academic360BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
